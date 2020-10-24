d93b31cbf88ae9dd624fa70cad761b820d72abc7
williams-max <almanzaisrael75@gmail.com>
2020-10-24 14:56:06 -0600

    se hizo una mejora para controlar las notificaciones , tambien se añadio un nuevo campo en la tabla User

--- app/Listeners/PostListerner.php
+++ app/Listeners/PostListerner.php
@@ -41,46 +41,102 @@ class PostListerner
        // });
         */
         //Aumentado codigo
+
         $valor=0;
+
+        
+        //$dia = Carbon\Carbon::now();
         $dia = Carbon::now(); 
-       // if($dia->isoFormat('dddd')=='Monday') {
+        //obtenemos el primer registro
+        $registro   =   User::where("id",1000)->first();
+        //se obtiene el primer registro del id admistrador
+        //$registro->id;
+        //obtenemos el emial del administrador
+       // $registro->email;
+       $users = User::all();
 
-            //Condigo para enviar notificaciones dentro la interfaz
+       //Monday
+       if($dia->isoFormat('dddd')=='Monday')
+        {
+
+      
+
+                 //Condigo para enviar notificaciones dentro la interfaz
             User::all()
-           // ->except($event->post->user_id)
-            ->each(function(User $user) use ($event){
-                Notification::send($user, new PostNotificacion($event->post));
-               // Notification::send($users, new InvoicePaid($invoice));
-    
+            // ->except($event->post->user_id)
+             ->each(function(User $user) use ($event){
+              if($user->email=='admin@gmail.com'){
+              }else{
+               if($user->enviado==0){
+                 Notification::send($user, new PostNotificacion($event->post));
+                  // Notification::send($users, new InvoicePaid($invoice)); 
+                }
+              }
             });
-             //codigo para enviar correos
-             /*
-            //Mail::send('emails.primeiro',[], function($message){ 
-             
-                $enderecos = ['almanzaisrael75@gmail.com','chevycheluis@gmail.com'];
-                 
-                $message->to($enderecos);
-                $message->subject('Notificacion');
-           // });
-              */ 
+              //codigo para enviar correos
+              /*
+             //Mail::send('emails.primeiro',[], function($message){ 
               
-              $users = User::all();
-
-              foreach($users as $user){
-                Mail::send('emails.primeiro',['user' => $user ], function($message) use ($user){ 
-             
-                    //$enderecos = ['almanzaisrael75@gmail.com','chevycheluis@gmail.com'];
-                     
-                    $message->to($user->email);
-                    $message->subject('Notificacion');
-                }); 
+                 $enderecos = ['almanzaisrael75@gmail.com','chevycheluis@gmail.com'];
                   
-              } 
+                 $message->to($enderecos);
+                 $message->subject('Notificacion');
+            // });
+               */ 
+             
+             foreach($users as $user){
+                 Mail::send('emails.primeiro',['user' => $user ], function($message) use ($user){ 
+                                  //$enderecos = ['almanzaisrael75@gmail.com','chevycheluis@gmail.com'];
+                      if($user->email=='admin@gmail.com')
+                      {
+                      }else
+                      {
+                        if($user->enviado==0){
+
+                      
+                          $message->to($user->email);
+                          $message->subject('Formulario');
+
+                          $user->enviado =1;
+                          $user->save();
+
+                        }
+                      }
+                });                   
+               } 
+        }else{
+           //dia miercoles verficamos si el usaario no envio el formulario 
+           //Saturday
+           //Wednesday
+           if($dia->isoFormat('dddd')=='Wednesday')
+           {
+            foreach($users as $user){
+             
+                   if($user->email=='admin@gmail.com')
+                   {
+                   }else
+                   {
+                     if($user->enviado==1){
+                       $user->enviado =0;
+                       $user->save();
+
+                     }
+                   }
+                               
+            } 
+
+           }
+       
+         
+
+        }
+
+           
 
           
         
 
-       // }
+     }
       
-    }
+ 
 }
