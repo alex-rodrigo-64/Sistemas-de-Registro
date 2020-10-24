39915198a280bef694d7e82574317c86338da7f8
williams-max <almanzaisrael75@gmail.com>
2020-10-24 17:24:32 -0600

    se hizo una mejora para controlar las notificaciones y se añadio un campo en la tabla User

--- resources/views/emails/primeiro.blade.php
+++ resources/views/emails/primeiro.blade.php
@@ -1,2 +1,3 @@
-<p>Se le recuerda que tiene que llenar el Formulario </p>
-<p>Registro y Avance de Materia !!!!</p>
+<h1>Hola , {{$user->name}} :</h1>
+<h4>Se le recuerda que tiene que llenar el Formulario </h4>
+<h4>Registro y Avance de Materia !!!!</h4>
